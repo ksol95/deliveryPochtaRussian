30a023035bb5c1532b3f3fbac1e9417b52b74896
KSOL <ksol95@ya.ru>
2024-05-15 18:59:45 +0400

    Исправил баг wrom - to

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,36 +2,38 @@ import { postCalculate } from "../components/api.js";
 
 const response = document.querySelector(".response");
 const deilveryForm = document.querySelector(".deilvery-info");
-const from = deilveryForm.from;
+const toIndex = deilveryForm.toIndex;
 const weight = deilveryForm.weight;
 const sumoc = deilveryForm.sumoc;
 
 let postConfig = {
   json: "",
   object: "4040",
-  service: "22,38,59",
+  service: "22",
   increment: 50, //Прибавляем к стоимости доставки
 
-  to: "445042", //Откуда
-  from: "352120", //Куда
+  to: "681000", //Куда
+  from: "445042", //Откуда
 
   weight: "2000", //Веc в граммах
   size: "85x18x9", //Размеры в сантиметрах
-  sumoc: "573000", //Сумма объявленной ценности с копейками
+  sumoc: "490000", //Сумма объявленной ценности с копейками
 };
 
 const сommission = (sum) => {
-  return sum <= 10000 ? 41.67 + (sum / 100 * 4.17) : 166.67 + (sum / 100 * 3.33);
+  return sum <= 10000
+    ? 41.67 + (sum / 100) * 4.17
+    : 166.67 + (sum / 100) * 3.33;
 };
 
 function formatNumber(num) {
-  return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')
+  return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
 }
 
 const calculate = () => {
   postConfig.weight = weight.value;
   postConfig.sumoc = sumoc.value * 100;
-  postConfig.from = from.value;
+  postConfig.to = toIndex.value;
 
   postCalculate(postConfig)
     .then((res) => {
@@ -47,11 +49,10 @@ const calculate = () => {
         sumoc: res.sumoc + res.paynds,
       };
       postCalculate(newConfig).then((res) => {
-
         console.log(res);
         response.innerHTML = "Загрузка...";
         let sum =
-          Math.round((res.paynds / 100 + postConfig.increment) / 10) * 10;
+          Math.round(res.paynds / 100 / 100) * 100 + postConfig.increment;
         let timeMin = res.delivery.min;
         let timeMax = res.delivery.max;
         let total = postConfig.sumoc / 100 + sum;
@@ -59,10 +60,28 @@ const calculate = () => {
         let elem = document.createElement("div");
         elem.classList.add("delivery-result");
         elem.innerHTML = `
-            <p>Стоимость доставки: ${formatNumber(sum)} руб.;</p>
-            <p>Срок доставки: от ${++timeMin} до ${++timeMax} дней;</p>
-            <p>Итого: ${formatNumber(total)} руб.;</p>
-            <p>(+ комиссия за денежный перевод через почту России: ${formatNumber(Math.round(сommission(total)))} руб.)</p>
+            <p>Детали расчёта (не для клиента)</p>
+            <ul>
+              <li><small>Тариф: ${Math.round(
+                res.ground.valnds / 100
+              )} руб.</small></li>
+              <li><small>Плата за объявленную ценность: ${
+                res.cover.valnds / 100
+              } руб.</small></li>
+              <li><small>Дополнительные услуги: ${
+                res.service.valnds / 100
+              } руб.</small></li>
+              <li><small>Допы: ${postConfig.increment} руб.</small></li>
+            </ul>
+            <p>Расчёт для клиента:</p>
+            <ul>
+              <li>Стоимость доставки: ${formatNumber(sum)} руб.;</li>
+              <li>Срок доставки: от ${++timeMin} до ${++timeMax} дней;</li>
+              <li>Итого: ${formatNumber(total)} руб.;</li>
+              <li>(+ комиссия за денежный перевод через почту России: ${formatNumber(
+                Math.round(сommission(total))
+              )} руб.)</li>
+            </ul>
           `;
         response.innerHTML = "";
         response.append(elem);
@@ -71,7 +90,7 @@ const calculate = () => {
     .catch((res) => {
       console.error(res);
       response.innerHTML = "";
-      response.innerHTML =`Ошибка: ${res}`;
+      response.innerHTML = `Ошибка: ${res}`;
     });
 };
 
@@ -84,5 +103,5 @@ deilveryForm.addEventListener("submit", handleSubmit);
 
 weight.value = postConfig.weight;
 sumoc.value = postConfig.sumoc / 100;
-from.value = postConfig.from;
+toIndex.value = postConfig.to;
 calculate();
